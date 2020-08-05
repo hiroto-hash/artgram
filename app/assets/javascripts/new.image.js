23e05772239f5405543365ea04984595d64082b9
hiroto-hash <hiroto5118@icloud.com>
2020-08-05 13:26:17 +0900

    商品出品ページ

---
+++ app/assets/javascripts/new.image.js
@@ -0,0 +1,24 @@
+
+$(function(){
+  $('#myfile').change(function(e){
+    
+    var file = e.target.files[0];
+    var reader = new FileReader();
+ 
+    
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+ 
+    
+    reader.onload = (function(file){
+      return function(e){
+        $("#img").attr("src", e.target.result);
+        $("#img").attr("title", file.name);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+ 
+  });
+});
